88f62d7627f850830f3965b75780d984ddaf458f
Yu Zhang <mzhangyjoy@gmail.com>
2023-06-03 00:01:04 -0500

    mapreduce worker implementation

--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -24,6 +24,39 @@ type ExampleReply struct {
 
 // Add your RPC definitions here.
 
+const (
+	// for taskType
+	WAIT_TASK   = 0
+	MAP_TASK    = 1
+	REDUCE_TASK = 2
+	NO_TASK     = 3
+
+	// for finish status
+	COMPLETED_TASK = 1
+	FAILED_TASK    = 2
+)
+
+type GetTaskArgs struct{}
+type ReplyGetTaskArgs struct {
+	taskType    int // 0: wait, 1: map, 2: reduce, ?3: no tasks
+	taskId      int
+	taskContent string // filename for map, bucket_no for reduce
+}
+
+type GetBucketInfoArgs struct{}
+
+type ReplyGetBucketInfoArgs struct {
+	bucketCount  int
+	mapTaskCount int
+}
+
+type FinishTaskArgs struct {
+	taskType   int // 1: map, 2: reduce
+	taskId     int
+	taskStatus int // 0: completed, -1: failed
+}
+
+type ReplyFinishTaskArgs struct{}
 
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the coordinator.
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -4,7 +4,11 @@ import "fmt"
 import "log"
 import "net/rpc"
 import "hash/fnv"
-
+import "time"
+import "os"
+import "io/ioutil"
+import "sort"
+import "encoding/json"
 
 //
 // Map functions return a slice of KeyValue.
@@ -24,6 +28,18 @@ func ihash(key string) int {
 	return int(h.Sum32() & 0x7fffffff)
 }
 
+// for sorting by key.
+type ByKey []KeyValue
+
+// for sorting by key.
+func (a ByKey) Len() int           { return len(a) }
+func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
+
+var bucketCount int
+var mapTaskCount int
+var taskType int
+var taskId int
 
 //
 // main/mrworker.go calls this function.
@@ -36,6 +52,143 @@ func Worker(mapf func(string, string) []KeyValue,
 	// uncomment to send the Example RPC to the coordinator.
 	// CallExample()
 
+	initArgs := GetBucketInfoArgs{}
+	initReply := ReplyGetBucketInfoArgs{}
+
+	call("Coordinator.InitTask", &initArgs, &initReply)
+	bucketCount = initReply.bucketCount
+	mapTaskCount = initReply.mapTaskCount
+
+	args := GetTaskArgs{}
+	reply := ReplyGetTaskArgs{}
+
+	for {
+		succeeded := call("Coordinator.GetTask", &args, &reply)
+		if !succeeded {
+			break
+		}
+		if taskType == WAIT_TASK {
+			time.Sleep(time.Second)
+			continue
+		}
+
+		taskType = reply.taskType
+		taskId = reply.taskId
+
+		taskStatus := -1
+
+		if taskType == MAP_TASK {
+			taskStatus = runMapTask(mapf, reply.taskContent)
+		} else {
+			taskStatus = runReduceTask(reducef, reply.taskContent)
+		}
+
+		finishArgs := FinishTaskArgs{
+			taskType:   taskType,
+			taskId:     taskId,
+			taskStatus: taskStatus,
+		}
+
+		replyFinishArgs := ReplyFinishTaskArgs{}
+
+		call("Coordinator.FinishTask", &finishArgs, &replyFinishArgs)
+	}
+
+}
+
+func runMapTask(mapf func(string, string) []KeyValue, filename string) int {
+	intermediate := []KeyValue{}
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("cannot open %v", filename)
+		return FAILED_TASK
+	}
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatalf("cannot read %v", filename)
+		return FAILED_TASK
+	}
+	file.Close()
+	kva := mapf(filename, string(content))
+	intermediate = append(intermediate, kva...)
+	ofiles := make([]*os.File, bucketCount)
+	fileEncoders := make([]*json.Encoder, bucketCount)
+	for i := 0; i < bucketCount; i++ {
+		oname := fmt.Sprintf("mr-%v-%v", taskId, i)
+		ofiles[i], err = os.Create(oname)
+		if err != nil {
+			log.Fatalf("cannot create %v", oname)
+			return FAILED_TASK
+		}
+		fileEncoders[i] = json.NewEncoder(ofiles[i])
+	}
+
+	for _, kv := range intermediate {
+		index := ihash(kv.Key) % bucketCount
+		err := fileEncoders[index].Encode(&kv)
+		if err != nil {
+			log.Fatalf("cannot encode %v", kv)
+			return FAILED_TASK
+		}
+	}
+
+	for i := 0; i < bucketCount; i++ {
+		ofiles[i].Close()
+	}
+
+	return COMPLETED_TASK
+}
+
+func runReduceTask(reducef func(string, []string) string, bucket_no string) int {
+	// since all files live on the same machine, we can just read the file
+	// for the implementation in the map reduce paper,
+	// all the input files to Reduce task are local to the machine running
+	// the corresponding mapper
+	// need to bookkeep machine info in coordinator, and pass this info
+	// to reduce task, so the reduce worker can contact the machines and
+	// get corresponding files
+
+	kva := []KeyValue{}
+	for i := 0; i < mapTaskCount; i++ {
+		filename := fmt.Sprintf("mv-%v-%v", i, bucket_no)
+		file, err := os.Open(filename)
+		if err != nil {
+			log.Fatalf("cannot open %v", file)
+			return FAILED_TASK
+		}
+		dec := json.NewDecoder(file)
+		for {
+			var kv KeyValue
+			if err := dec.Decode(&kv); err != nil {
+				break
+			}
+			kva = append(kva, kv)
+		}
+		file.Close()
+	}
+	sort.Sort(ByKey(kva))
+	i := 0
+	tmpfile, err := ioutil.TempFile("", "tmp")
+	if err != nil {
+		log.Fatalf("cannot create temp file")
+	}
+	for i < len(kva) {
+		j := i + 1
+		for j < len(kva) && kva[j].Key == kva[i].Key {
+			j++
+		}
+		values := []string{}
+		for k := i; k < j; k++ {
+			values = append(values, kva[k].Value)
+		}
+		output := reducef(kva[i].Key, values)
+
+		fmt.Fprintf(tmpfile, "%v %v\n", kva[i].Key, output)
+		i = j
+	}
+	oFilename := fmt.Sprintf("mv-out-%v", taskId)
+	os.Rename(tmpfile.Name(), oFilename)
+	return COMPLETED_TASK
 }
 
 //
